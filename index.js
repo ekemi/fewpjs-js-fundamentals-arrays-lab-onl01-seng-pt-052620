fb28edb96d527e926c6c391d114ee90b6098c8d3
edmundo papi <edmundopierre@yahoo.com>
2020-11-23 13:50:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your solution here!
+append = ["Milo", "Otis", "Garfield"]
+prepend = ["Milo", "Otis", "Garfield"]
+removeLast = ["Milo", "Otis", "Garfield"]
+removeFirst = ["Milo", "Otis", "Garfield"]
+append.push("Odie");
